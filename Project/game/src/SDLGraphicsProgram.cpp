b1f3f57f4ff31a247b03c28b379e7084dd65fb82
Dylan Horgan <dyhorgan@gmail.com>
2023-11-28 01:05:21 -0500

    cleaned up unpushed commits

---
+++ Project/game/src/SDLGraphicsProgram.cpp
@@ -0,0 +1,405 @@
+#include "SDLGraphicsProgram.hpp"
+#include "Camera.hpp"
+#include "Terrain.hpp"
+#include "Sphere.hpp"
+#include "Canvas.hpp"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+// Initialization function
+// Returns a true or false value based on successful completion of setup.
+// Takes in dimensions of window.
+SDLGraphicsProgram::SDLGraphicsProgram(int w, int h){
+	// Initialization flag
+	bool success = true;
+	// String to hold any errors that occur.
+	std::stringstream errorStream;
+	// The window we'll be rendering to
+	m_window = NULL;
+
+	// Initialize SDL
+	if(SDL_Init(SDL_INIT_VIDEO)< 0){
+		errorStream << "SDL could not initialize! SDL Error: " << SDL_GetError() << "\n";
+		success = false;
+	}
+	else{
+		//Use OpenGL 3.3 core
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
+		SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+		// We want to request a double buffer for smooth updating.
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+		//Create window
+		m_window = SDL_CreateWindow( "Lab",
+                                SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED,
+                                w,
+                                h,
+                                SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+
+		// Check if Window did not create.
+		if( m_window == NULL ){
+			errorStream << "Window could not be created! SDL Error: " << SDL_GetError() << "\n";
+			success = false;
+		}
+
+		//Create an OpenGL Graphics Context
+		m_openGLContext = SDL_GL_CreateContext( m_window );
+		if( m_openGLContext == NULL){
+			errorStream << "OpenGL context could not be created! SDL Error: " << SDL_GetError() << "\n";
+			success = false;
+		}
+
+		// Initialize GLAD Library
+		if(!gladLoadGLLoader(SDL_GL_GetProcAddress)){
+			errorStream << "Failed to iniitalize GLAD\n";
+			success = false;
+		}
+
+		//Initialize OpenGL
+		if(!InitGL()){
+			errorStream << "Unable to initialize OpenGL!\n";
+			success = false;
+		}
+  	}
+
+    // If initialization did not work, then print out a list of errors in the constructor.
+    if(!success){
+        errorStream << "SDLGraphicsProgram::SDLGraphicsProgram - Failed to initialize!\n";
+        std::string errors=errorStream.str();
+        SDL_Log("%s\n",errors.c_str());
+    }else{
+        SDL_Log("SDLGraphicsProgram::SDLGraphicsProgram - No SDL, GLAD, or OpenGL, errors detected during initialization\n\n");
+    }
+
+	// SDL_LogSetAllPriority(SDL_LOG_PRIORITY_WARN); // Uncomment to enable extra debug support!
+	GetOpenGLVersionInfo();
+
+
+    // Setup our Renderer
+    m_renderer = new Renderer(w,h);  
+    //background_renderer = new Renderer(w,h);  
+}
+
+
+// Proper shutdown of SDL and destroy initialized objects
+SDLGraphicsProgram::~SDLGraphicsProgram(){
+    if(m_renderer!=nullptr){
+        delete m_renderer;
+    } 
+    /* 
+    if(background_renderer!=nullptr){
+        delete background_renderer;
+    }
+    */
+
+
+    //Destroy window
+	SDL_DestroyWindow( m_window );
+	// Point m_window to NULL to ensure it points to nothing.
+	m_window = nullptr;
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+
+// Initialize OpenGL
+// Setup any of our shaders here.
+bool SDLGraphicsProgram::InitGL(){
+	//Success flag
+	bool success = true;
+
+	return success;
+}
+
+// ====================== Create the planets =============
+// NOTE: I will admit it is a little lazy to have these as globals,
+//       we could build on our ObjectManager structure to manage the
+//       each object that is inserted and automatically create a 'SceneNode'
+//       for it for example. Or otherwise, build a 'SceneTree' class that
+//       manages creating nodes and setting their type.
+//       (e.g. A good use of the Factory Design Pattern if you'd like to
+//             invetigate further--but it is beyond the scope of this assignment).
+
+// Create the Moon
+Object* panel3;
+SceneNode* Brick3;
+// Create the Earth
+Object* panel2;
+SceneNode* Brick2;
+// Create the Sun
+Object* panel;
+SceneNode* Brick1;
+
+Object* canvas;
+SceneNode* background;
+
+Object* sprite;
+SceneNode* character;
+
+Object* panel4;
+SceneNode* Brick4;
+
+Object* sphere;
+SceneNode* Money;
+
+// ====================== Create the planets =============
+
+//Loops forever!
+void SDLGraphicsProgram::Loop(){
+
+    // ================== Initialize the planets ===============
+    static float rotate = 0.05f;
+    float moonRotate = 0.05;
+
+    // Create new geometry for Earth's Moon
+    sphere = new Sphere();
+    sphere->LoadTexture("dollarsigns.ppm");
+    // Create a new node using sphere3 as the geometry
+    Money = new SceneNode(sphere);
+
+    // Create new geometry for Earth's Moon
+    panel4 = new Canvas();
+    panel4->LoadTexture("brick.ppm");
+    // Create a new node using sphere3 as the geometry
+    Brick4 = new SceneNode(panel4);
+
+    // Create new geometry for Earth's Moon
+    panel3 = new Canvas();
+    panel3->LoadTexture("brick.ppm");
+    // Create a new node using sphere3 as the geometry
+    Brick3 = new SceneNode(panel3);
+
+    // Create the Earth
+    panel2 = new Canvas();
+    panel2->LoadTexture("brick.ppm");
+    Brick2 = new SceneNode(panel2);
+
+    // Create the Sun
+    panel = new Canvas();
+    panel->LoadTexture("brick.ppm");
+    Brick1 = new SceneNode(panel);
+    
+    canvas = new Canvas();
+    canvas->LoadTexture("blue.ppm");
+    background = new SceneNode(canvas);
+
+    sprite = new Canvas();
+    sprite->LoadTexture("brock.ppm");
+    character = new SceneNode(sprite);
+
+    //background = new
+
+    // Render our scene starting from the sun.
+    m_renderer->setRoot(background);
+    // Make the Earth a child of the Sun
+    background->AddChild(Brick1);
+    background->AddChild(character);
+
+    Brick1->AddChild(Brick2);
+    // Make the Moon a child of the Earth
+    Brick2->AddChild(Brick3);
+
+    Brick3->AddChild(Brick4);
+
+    Brick4->AddChild(Money);
+    
+
+    //background_renderer->setRoot(background);
+    
+    // Set a default position for our camera
+    m_renderer->GetCamera(0)->SetCameraEyePosition(-1.0f,-0.5f,19.0f);
+
+    // Main loop flag
+    // If this is quit = 'true' then the program terminates.
+    bool quit = false;
+    // Event handler that handles various events in SDL
+    // that are related to input and output
+    SDL_Event e;
+    // Enable text input
+    SDL_StartTextInput();
+
+    // Set the camera speed for how fast we move.
+    float cameraSpeed = 5.0f;
+
+    // While application is running
+    while(!quit){
+
+        //Handle events on queue
+        while(SDL_PollEvent( &e ) != 0){
+            // User posts an event to quit
+            // An example is hitting the "x" in the corner of the window.
+            if(e.type == SDL_QUIT){
+                quit = true;
+            }
+            if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE){
+			    std::cout << "ESC: Goodbye! (Leaving MainApplicationLoop())" << std::endl;
+                quit = true;
+            }
+            // Handle keyboard input for the camera class
+            if(e.type==SDL_MOUSEMOTION){
+                // Handle mouse movements
+                int mouseX = e.motion.x;
+                int mouseY = e.motion.y;
+                //m_renderer->GetCamera(0)->MouseLook(mouseX, mouseY);
+            }
+
+            /*
+            switch(e.type){
+                // Handle keyboard presses
+                case SDL_KEYDOWN:
+                    switch(e.key.keysym.sym){
+                        case SDLK_LEFT:
+                            m_renderer->GetCamera(0)->MoveLeft(cameraSpeed);
+                            break;
+                        case SDLK_RIGHT:
+                            m_renderer->GetCamera(0)->MoveRight(cameraSpeed);
+                            break;
+                        case SDLK_UP:
+                            m_renderer->GetCamera(0)->MoveForward(cameraSpeed);
+                            break;
+                        case SDLK_DOWN:
+                            m_renderer->GetCamera(0)->MoveBackward(cameraSpeed);
+                            break;
+                        case SDLK_RSHIFT:
+                            m_renderer->GetCamera(0)->MoveUp(cameraSpeed);
+                            break;
+                        case SDLK_RCTRL:
+                            m_renderer->GetCamera(0)->MoveDown(cameraSpeed);
+                            break;
+                    }
+                break;
+            }
+            */
+        } 
+
+        // Retrieve keyboard state
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    /*
+    if (state[SDL_SCANCODE_UP]) {
+        g_uOffset+=0.01f;
+        //std::cout << "g_uOffset: " << g_uOffset << std::endl;
+    }
+    if (state[SDL_SCANCODE_DOWN]) {
+        g_uOffset-=0.01f;
+        //std::cout << "g_uOffset: " << g_uOffset << std::endl;
+    }
+    if (state[SDL_SCANCODE_LEFT]) {
+        g_uRotate-=1.0f;
+        //std::cout << "g_uRotate: " << g_uRotate<< std::endl;
+    }
+    if (state[SDL_SCANCODE_RIGHT]) {
+        g_uRotate+=1.0f;
+        //std::cout << "g_uRotate: " << g_uRotate<< std::endl;
+    }
+    */
+
+    // Camera
+    // Update our position of the camera
+    if (state[SDL_SCANCODE_W]) {
+        //m_renderer->GetCamera(0)->MoveForward(0.1f);
+        m_renderer->GetCamera(0)->MoveUp(0.1f);
+    }
+    if (state[SDL_SCANCODE_S]) {
+        //m_renderer->GetCamera(0)->MoveBackward(0.1f);
+        m_renderer->GetCamera(0)->MoveDown(0.1f);
+    }
+    
+    if (state[SDL_SCANCODE_A]) {
+        m_renderer->GetCamera(0)->MoveLeft(0.1f);
+    }	
+    if (state[SDL_SCANCODE_D]) {
+       m_renderer->GetCamera(0)->MoveRight(0.1f);
+    }
+    
+    if (state[SDL_SCANCODE_UP]) {
+        m_renderer->GetCamera(0)->MoveForward(0.1f);
+    }
+	if (state[SDL_SCANCODE_DOWN]) {
+        m_renderer->GetCamera(0)->MoveBackward(0.1f);
+    }
+    
+    
+
+        // End SDL_PollEvent loop.
+        // ================== Use the planets ===============
+        // TODO:
+        //      After the planets have been created, and the hierarchy
+        //      has been made, you can transform them.
+        //      Note, you'll need to setup the scene graph world/local
+        //      transformation correctly first before the planets are shown.
+        //      
+        //      The 'Sun' for example will be the only object shown initially
+        //      since the rest of the planets are children (or grandchildren)
+        //      of the Sun.
+       
+        background->GetLocalTransform().LoadIdentity();
+        background->GetLocalTransform().Scale(16.0,12.0,12.0);
+
+        Brick1->GetLocalTransform().LoadIdentity();
+        Brick1->GetLocalTransform().Translate(-0.2,-0.1,1.15);
+        Brick1->GetLocalTransform().Scale(0.04,0.035,0.05);	
+        // ... transform the Sun
+
+        Brick3->GetLocalTransform().LoadIdentity();	
+        
+        // ... transform the Moon
+        Brick3->GetLocalTransform().Translate(4.0,0.0,0.0);
+    
+        	
+        Brick2->GetLocalTransform().LoadIdentity();		
+        // ... transform the Earth
+        Brick2->GetLocalTransform().Translate(2.0,0.0,0.0);
+
+        Brick4->GetLocalTransform().LoadIdentity();	
+        // ... transform the Moon
+        Brick4->GetLocalTransform().Translate(2.0,2.0,0.0);
+
+        Money->GetLocalTransform().LoadIdentity();
+        Money->GetLocalTransform().Scale(0.5, 0.6, 0.6);
+        Money->GetLocalTransform().Translate(0.0, 3.0, 0.0);
+        Money->GetLocalTransform().Rotate(rotate, 0.0, 1.0, 0.0);
+
+        character->GetLocalTransform().LoadIdentity();
+        character->GetLocalTransform().Scale(0.02, 0.05, 0.05);
+        character->GetLocalTransform().Translate(-9.0,-0.4,23);	
+        
+        //background_renderer->Update();
+        //background_renderer->Render();
+        // Update our scene through our renderer
+        m_renderer->Update();
+        // Render our scene using our selected renderer
+        m_renderer->Render();
+
+
+        // Delay to slow things down just a bit!
+        SDL_Delay(25);  // TODO: You can change this or implement a frame
+                        // independent movement method if you like.
+      	//Update screen of our specified window
+      	SDL_GL_SwapWindow(GetSDLWindow());
+        rotate += 0.03;
+        moonRotate += 0.15;
+	}
+    //Disable text input
+    SDL_StopTextInput();
+}
+
+
+// Get Pointer to Window
+SDL_Window* SDLGraphicsProgram::GetSDLWindow(){
+  return m_window;
+}
+
+// Helper Function to get OpenGL Version Information
+void SDLGraphicsProgram::GetOpenGLVersionInfo(){
+	SDL_Log("(Note: If you have two GPU's, make sure the correct one is selected)");
+	SDL_Log("Vendor: %s",(const char*)glGetString(GL_VENDOR));
+	SDL_Log("Renderer: %s",(const char*)glGetString(GL_RENDERER));
+	SDL_Log("Version: %s",(const char*)glGetString(GL_VERSION));
+	SDL_Log("Shading language: %s",(const char*)glGetString(GL_SHADING_LANGUAGE_VERSION));
+}
